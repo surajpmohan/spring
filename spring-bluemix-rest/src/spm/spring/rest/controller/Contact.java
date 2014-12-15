ea9ead792297b35ec44c80b41829be8141ff6b77
surajpmohan <surajpmohan@gmail.com>
2014-12-15 14:07:13 +0530

    commit

--- spring-bluemix-rest/src/spm/spring/rest/controller/Contact.java
+++ spring-bluemix-rest/src/spm/spring/rest/controller/Contact.java
@@ -1,7 +1,8 @@
 package spm.spring.rest.controller;
 
-import java.util.Date;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,30 +10,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import spm.spring.rest.bean.ContactBean;
+import spm.spring.rest.data.ContactDao;
 
 @Controller
 @RequestMapping("/contact")
 public class Contact {
+	@Autowired
+	private ContactDao contactDao;
+	public ContactDao getContactDao() {
+		return contactDao;
+	}
+	public void setContactDao(ContactDao contactDao) {
+		this.contactDao = contactDao;
+	}
+	
 	@RequestMapping(method=RequestMethod.GET,produces="application/json")
-	public @ResponseBody ContactBean get(){
-		ContactBean bean = new ContactBean();
-		bean.setDob(new Date());
-		bean.setEmail("s@abc.com");
-		bean.setFirstName("Andand");
-		//bean.setId(id);
-		bean.setLastName("adnanan");
-		bean.setPhone("123456789");
-		return bean;
+	public @ResponseBody List<ContactBean> get(){
+		return contactDao.getAll();
 	}
 	@RequestMapping(method=RequestMethod.GET, value="/{id}",produces="application/json")
 	public @ResponseBody ContactBean get(@PathVariable int id){
-		ContactBean bean = new ContactBean();
-		bean.setDob(new Date());
-		bean.setEmail("s@abc.com");
-		bean.setFirstName("Andand");
-		bean.setId(id);
-		bean.setLastName("adnanan");
-		bean.setPhone("123456789");
-		return bean;
+		return contactDao.get(id);
 	}
 }
--- spring-bluemix-rest/src/spm/spring/rest/data/ContactDao.java
+++ spring-bluemix-rest/src/spm/spring/rest/data/ContactDao.java
@@ -1,5 +1,13 @@
 package spm.spring.rest.data;
 
+import java.util.List;
+
+import spm.spring.rest.bean.ContactBean;
+
 public interface ContactDao {
-	private 
+	public ContactBean get(int id);
+	public List<ContactBean> getAll();
+	public ContactBean create(ContactBean contact);
+	public int delete(int id);
+	public int update(ContactBean contact);
 }
