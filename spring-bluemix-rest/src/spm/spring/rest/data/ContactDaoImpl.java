fc9ffd6b64fe6f70b0de5d32e36ab7c47cc62c33
surajpmohan <surajpmohan@gmail.com>
2014-12-15 16:22:38 +0530

    updated

--- spring-bluemix-rest/src/spm/spring/rest/data/ContactDaoImpl.java
+++ spring-bluemix-rest/src/spm/spring/rest/data/ContactDaoImpl.java
@@ -1,5 +1,7 @@
 package spm.spring.rest.data;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -7,8 +9,13 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import spm.spring.rest.bean.ContactBean;
@@ -22,8 +29,32 @@ public class ContactDaoImpl implements ContactDao {
 	}
 	@Override
 	public ContactBean get(int id) {
+		try {
+			String sql = "SELECT ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE, DOB FROM SPRING.CONTACT WHERE ID=?";
+			return jdbcTemplate.queryForObject(sql, new Object[]{id}, new RowMapper<ContactBean>() {
+				@Override
+				public ContactBean mapRow(ResultSet rs, int arg1)
+						throws SQLException {
+					ContactBean contact = new ContactBean();
+					contact.setDob(rs.getDate("DOB"));
+					contact.setEmail(rs.getString("EMAIL"));
+					contact.setFirstName(rs.getString("FIRST_NAME"));
+					contact.setId(rs.getInt("ID"));
+					contact.setLastName(rs.getString("LAST_NAME"));
+					contact.setPhone(rs.getString("PHONE"));
+					return contact;
+				}
+			});
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public List<ContactBean> getAll() {
 		String sql = "SELECT ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE, DOB FROM SPRING.CONTACT";
-		return jdbcTemplate.queryForObject(sql, new Object[]{id}, new RowMapper<ContactBean>() {
+		return jdbcTemplate.query(sql, new RowMapper<ContactBean>() {
 
 			@Override
 			public ContactBean mapRow(ResultSet rs, int arg1)
@@ -41,23 +72,36 @@ public class ContactDaoImpl implements ContactDao {
 	}
 
 	@Override
-	public List<ContactBean> getAll() {
-		return null;
-	}
-
-	@Override
-	public ContactBean create(ContactBean contact) {
-		return null;
+	public ContactBean create(final ContactBean contact) {
+		final String sql = "INSERT INTO  SPRING.CONTACT (FIRST_NAME, LAST_NAME, EMAIL, PHONE, DOB)  VALUES (?,?,?,?,?)";
+		KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con)
+					throws SQLException {
+				PreparedStatement statement = con.prepareStatement(sql, new String[]{"ID"});
+				statement.setString(1, contact.getFirstName());
+				statement.setString(2, contact.getLastName());
+				statement.setString(3, contact.getEmail());
+				statement.setString(4, contact.getPhone());
+				statement.setDate(5, new java.sql.Date(contact.getDob().getTime()));
+				return statement;
+			}
+		}, generatedKeyHolder);
+		contact.setId(generatedKeyHolder.getKey().intValue());
+		return contact;
 	}
 
 	@Override
 	public int delete(int id) {
-		return 0;
+		String sql = "DELETE FROM SPRING.CONTACT WHERE ID=?";
+		return jdbcTemplate.update(sql, id);
 	}
 
 	@Override
 	public int update(ContactBean contact) {
-		return 0;
+		final String sql = "UPDATE SPRING.CONTACT SET (FIRST_NAME, LAST_NAME, EMAIL, PHONE, DOB)  = (?,?,?,?,?) WHERE ID=?";
+		return jdbcTemplate.update(sql, contact.getFirstName(), contact.getLastName(), contact.getEmail(),  contact.getPhone(), contact.getDob(), contact.getId());
 	}
 
 }
